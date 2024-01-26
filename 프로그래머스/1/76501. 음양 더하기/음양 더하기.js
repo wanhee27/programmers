d8e52e9a753fc5c7877a6edf8a6b4a9c95b423cc
Wh <154817006+wanhee27@users.noreply.github.com>
2024-01-26 09:45:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,7 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    for(let i = 0; i<absolutes.length; i++){
+       answer = signs[i] ? answer+absolutes[i] : answer-absolutes[i]
+ } 
+    return answer
+}
