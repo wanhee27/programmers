70a05f4ff76730d92290b0d21fd1978fcb7f92e9
Wh <154817006+wanhee27@users.noreply.github.com>
2024-01-30 09:51:21 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    if(arr.length<=1){
+        return [-1]
+    } else {
+        let min = Math.min(...arr)
+        let index = arr.indexOf(min)
+        arr.splice(index,1)
+    }return arr;
+}
