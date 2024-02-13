c6247e4149824d5b05d33fe311af6494fee5669d
Wh <154817006+wanhee27@users.noreply.github.com>
2024-02-13 09:50:35 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    if( s.length === 4 || s.length === 6){
+       for(let i =0; i<s.length; i++){
+           if(isNaN(s[i])){
+               return false
+           }}
+       
+       return true;}
+else{
+    return false
+}}
+
+
